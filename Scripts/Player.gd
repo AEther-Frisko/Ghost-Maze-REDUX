8f5aa95a0b37c2a72f8ed17f13f7dc6bcfd86dae
max <ajvwolf@gmail.com>
2024-03-07 12:19:36 -0500

    basic character controller created

---
+++ Scripts/Player.gd
@@ -0,0 +1,46 @@
+extends CharacterBody3D
+
+@onready var head = $Head
+
+const WALK_SPEED = 5.0
+const RUN_SPEED = 8.0
+
+const MOUSE_SENS = 0.4
+
+var currentSpeed = WALK_SPEED
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(deg_to_rad(-event.relative.x * MOUSE_SENS))
+		head.rotate_x(deg_to_rad(-event.relative.y * MOUSE_SENS))
+	if event.is_action_pressed("quit"):
+		get_tree().quit()
+
+func _physics_process(delta):
+	if Input.is_action_pressed("sprint"):
+		currentSpeed = RUN_SPEED
+	else:
+		currentSpeed = WALK_SPEED
+	
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backwards")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * currentSpeed
+		velocity.z = direction.z * currentSpeed
+	else:
+		velocity.x = move_toward(velocity.x, 0, currentSpeed)
+		velocity.z = move_toward(velocity.z, 0, currentSpeed)
+
+	move_and_slide()

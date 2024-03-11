3e1d70f8806a412c4592aaa67b202ab484a6358b
max <ajvwolf@gmail.com>
2024-03-11 02:51:45 -0400

    added door model and door sfx

--- Scripts/Door.gd
+++ Scripts/Door.gd
@@ -1,10 +1,14 @@
 extends MeshInstance3D
 
 @onready var interactionArea: InteractionArea = $InteractionArea
+@onready var doorSFX = $DoorSounds
 
 func _ready():
 	interactionArea.interact = Callable(self, "_onInteract")
 
 func _onInteract():
+	doorSFX.pitch_scale = randf_range(0.8, 1.2)
+	doorSFX.play()
+	await doorSFX.finished
 	var mapNumber = str(randi_range(1, 10))
 	SceneSwitcher.switchScene("res://Scenes/Maps/Level" + mapNumber + ".tscn")
